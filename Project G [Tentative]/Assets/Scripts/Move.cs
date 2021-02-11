0b74607b21821c45450bbf086a96cfd4a6c009d9
kawbro <kwicks@smu.edu>
2021-02-11 12:56:14 -0600

    Semester Project Prototype 1

---
+++ Project G [Tentative]/Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal"); //GetAxis(Horiz) checks if using A & D
+        float zDirection = Input.GetAxis("Vertical"); //GetAxis(Vert) checks if using W & S
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection); //xDir = forward/back, y = 0.0f (jump), zDir = Side to Side
+
+        transform.position += moveDirection;
+    }
+}
---
+++ Project G [Tentative]/Assets/Scripts/PCScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    //private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    //private float gravityValue = -9.81f;
+
+    public float speed = 5;
+    public float gravity = -5;
+
+    float velocityY = 0;
+
+    //CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        velocityY += gravity * Time.deltaTime;
+
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        input = input.normalized;
+
+        Vector3 temp = Vector3.zero;
+        if (input.z == 1)
+        {
+            temp += transform.forward;
+        }
+        else if (input.z == -1)
+        {
+            temp += transform.forward * -1;
+        }
+
+        if (input.x == 1)
+        {
+            temp += transform.right;
+        }
+        else if (input.x == -1)
+        {
+            temp += transform.right * -1;
+        }
+
+        Vector3 velocity = temp * speed;
+        velocity.y = velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            velocityY = 0;
+        }
+    }
+}
