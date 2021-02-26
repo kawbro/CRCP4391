1c45f955291101d0f4b4d103a9f2e249b0ce02f9
kawbro <kwicks@smu.edu>
2021-02-25 19:24:08 -0600

    Prototype 2 Final Commit

--- Project G [Tentative]/Assets/Scripts/NeoPlayerMovement.cs
+++ Project G [Tentative]/Assets/Scripts/NeoPlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+ * using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -37,3 +38,4 @@ public class NeoPlayerMovement : MonoBehaviour
 
     }
 }
+*/
