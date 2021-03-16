7a80f307e7586d2ef674a5d0ae6e1494cc6b6121
kawbro <kwicks@smu.edu>
2021-03-16 14:14:56 -0500

    3/16 commit 1.5

--- Project G [Tentative]/Assets/NeoPlayerMovement.cs
+++ Project G [Tentative]/Assets/NeoPlayerMovement.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class NeoPlayerMovement : MonoBehaviour
 {
     //private PlayerAnimation player_Animation
-    private gameObject.GetComponent<RigidBody>() myBody;
+    private Rigidbody myBody;
+    public float speed = 3.0f;
+    //private Rigid
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myBody = getComponent<Rigidbody>();
+        //myBody = getComponent<RigidBody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
     }
 }
