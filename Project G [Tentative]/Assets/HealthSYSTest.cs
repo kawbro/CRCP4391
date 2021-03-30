ff421ba4ffc8f8c593d31a52d1935fc7e19ebfba
kawbro <kwicks@smu.edu>
2021-03-30 15:16:57 -0500

    Re-commit: Health Script Updated

---
+++ Project G [Tentative]/Assets/HealthSYSTest.cs
@@ -0,0 +1,30 @@
+﻿//This code needs to be configured to the player but right now for the purpose of working on things it's a seperate script
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSYSTest : MonoBehaviour
+{
+    //Change to private ints when transistioned to player specific script
+    public int currentHealth; //Current health for player
+    public int totalHealth; //Total health for player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalHealth = 100;
+        currentHealth = totalHealth;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            currentHealth -= 20;
+            Debug.Log(gameObject.name + " " + currentHealth);
+        }
+    }
+}
